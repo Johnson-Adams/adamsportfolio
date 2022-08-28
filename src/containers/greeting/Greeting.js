4fd4499d36f77bf1a4005897d129fb8e336c7811
adams <adamsawesome326@gmail.com>
2022-08-28 17:18:18 -0400

    first commit

--- src/containers/greeting/Greeting.js
+++ src/containers/greeting/Greeting.js
@@ -49,8 +49,8 @@ export default function Greeting(props) {
                 className="greeting-text-span subTitle"
                 style={{ color: theme.text }}>
                 <div className="entities">
-                  An aspiring full-stack <mark data-entity="noun">Robotics Developer</mark>{" "}
-                  looking to <mark data-entity="verb">enhance capabilities</mark> of Autonomous Systems with my diverse <mark data-entity="skill">problem solving</mark> exposure across several domains.
+                  A full-stack <mark data-entity="noun">Robotics Developer</mark>{" "}
+                  looking to <mark data-entity="verb">enhance capabilities</mark> of Autonomous Systems with my diverse <mark data-entity="skill">problem solving</mark> exposure across multiple domains.
                 </div>
               </span>
               <CompetitiveSites logos={competitiveSites.competitiveSites} />
